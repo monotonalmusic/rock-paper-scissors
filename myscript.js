08bb46dc24fa4105e3e5180cc5a29d28a0e4d2d9
Gabe <gehughes28@gmail.com>
2022-08-21 16:41:15 +0200

    Made many changes including:
    Changed getComputerChoice to now use switch depending on result from 0-2.
    Function playRound, made case insensitive by adding .toLowerCase to both playerSelection & computerSelection.
    Made a series of if, else if, and else to declare winner for game depending on outcome.
    Next objective will be to figure out how to display the winner of playRound function in console.

--- myscript.js
+++ myscript.js
@@ -5,17 +5,47 @@ Declare a winner depending on results*/
 
 
 
-function playRound(playerSelection, computerSelection) {
-
-} 
 
 const playerSelection = "rock";
-const computerSelection = getComputerChoice();
+const rps = Array("rock", "paper", "scissors");
+let roundWinner = " ";
+let computerSelection = getComputerChoice().toLowerCase();
+console.log(computerSelection);
 
-
-function getComputerChoice(str) {
-    var rps = Array('rock', 'paper', 'scissors');
-    rps = (str[Math.floor(Math.random() * str.length)]);
+function getComputerChoice() {
+    let randomNumber = Math.floor(Math.random() * 3)
+        switch (randomNumber) {
+          case 0:
+            return 'rock'
+          case 1:
+            return 'paper'
+          case 2:
+            return 'scissors'
+        }
 }
 
-console.log(getComputerChoice)
+function playRound (playerSelection, computerSelection) {
+    computerSelection = getComputerChoice().toLowerCase();
+    playerSelection = playerSelection().toLowerCase();
+
+    if (playerSelection === computerSelection)
+    {
+        roundWinner = "tie";
+    }
+    else if (
+        (computerSelection == "rock" && playerSelection == "scissors") ||
+        (computerSelection == "paper" && playerSelection == "rock") ||
+        (computerSelection == "scissors" && playerSelection == "paper")
+    ){
+        roundWinner = "computer";
+    }
+
+   else if (
+        (computerSelection == "rock" && playerSelection == "paper") ||
+        (computerSelection == "paper" && playerSelection == "scissors") ||
+        (computerSelection == "scissors" && playerSelection == "rock")
+    ){
+        roundWinner = "player";
+    }
+}
+
