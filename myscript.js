cd57da64ff725b372747213d6c5c7d7364d09c60
Gabe <gehughes28@gmail.com>
2022-08-22 17:29:51 +0200

    Made game function
    In game function, made Array
    Made loop to run playRound 5x
    Push roundWinner to add to Array
    Array filter functions to sort and display results
    Moved variables from top to bottom, under game function

--- myscript.js
+++ myscript.js
@@ -3,16 +3,7 @@ Write a function to play single round of game
 There will be two selections for rock, paper, or scissors, one by player, one by computer
 Declare a winner depending on results*/
 
-
-
-
-var playerChoice = "rock";
-let computerChoice = getComputerChoice();
-let roundWinner = playRound(playerChoice.toLowerCase(), computerChoice.toLowerCase());
-let returnMessage = updateScore(roundWinner, playerChoice, computerChoice);
-console.log("player " + playerChoice);
-console.log("computer " + computerChoice);
-console.log(returnMessage);
+game();
 
 function getComputerChoice() {
     let randomNumber = Math.floor(Math.random() * 3)
@@ -48,6 +39,8 @@ function playRound (playerSelection, computerSelection) {
     ){
         return "Player wins!";
     }
+
+    
 }
 
 //Parameters: Takes roundWinner after running playRound
@@ -66,14 +59,36 @@ function capitalizeFirstLetter (str) {
     return str.charAt(0).toUpperCase() + str.slice(1).toLowerCase() 
 }
 
+function getPlayerChoice () {
+    return "rock"
+}
+
 //Parameters: None
 //Description: Call function game, to play playRound 5 times, keep score of player & computer, display score at the end of 5 rounds
-//Return: Results of 5 rounds as a string
+//Return: Results of 5 rounds as an array of strings
 
 function game() {
-    //Make a loop
-    
-    //Call playRound function
+    winnerArray = []
+    for (let i=0; i < 5; i++){
+        let playerChoice = getPlayerChoice();
+        let computerChoice = getComputerChoice();
+        let roundWinner = playRound(playerChoice.toLowerCase(), computerChoice.toLowerCase());
+        let returnMessage = updateScore(roundWinner, playerChoice, computerChoice);
+        console.log(returnMessage)
+        //Pushes result of playRound into winnerArray above
+        winnerArray.push(roundWinner)
+       
+    }
+
+    numOfPlayerWins = winnerArray.filter(x => x === "Player wins!").length
+    console.log("Player: " + numOfPlayerWins)
+
+    numOfComputerWins = winnerArray.filter(x => x === "Computer wins!").length
+    console.log("Computer: " + numOfComputerWins)
+
+    //First tally number of player wins
+    //Then tally number of computer wins
+    //If player is greater than computer, player wins and vice versa
     //Store the winner of each playRound function
     //Return a score board
 }
