db19db58f328543cfc497101fb63f6e4bb980b74
Gabe <gehughes28@gmail.com>
2022-08-31 18:43:14 +0200

    Made many changes, added pictures
    Made user interface for rock paper scissors, tallied score, alerted the winner. Big success!

--- myscript.js
+++ myscript.js
@@ -3,8 +3,6 @@ Write a function to play single round of game
 There will be two selections for rock, paper, or scissors, one by player, one by computer
 Declare a winner depending on results*/
 
-game();
-
 function getComputerChoice() {
     let randomNumber = Math.floor(Math.random() * 3)
         switch (randomNumber) {
@@ -16,6 +14,10 @@ function getComputerChoice() {
             return "scissors"
         }
 }
+
+let computerScore = 0;
+let playerScore = 0;
+
 
 //Parameters: Takes playerSelection and computerSelection as a string
 //Return: String of winner
@@ -29,6 +31,7 @@ function playRound (playerSelection, computerSelection) {
         (computerSelection == "paper" && playerSelection == "rock") ||
         (computerSelection == "scissors" && playerSelection == "paper")
     ){
+        computerScore = computerScore + 1
         return "Computer wins!";
     }
 
@@ -37,59 +40,60 @@ function playRound (playerSelection, computerSelection) {
         (computerSelection == "paper" && playerSelection == "scissors") ||
         (computerSelection == "scissors" && playerSelection == "rock")
     ){
+        playerScore = playerScore + 1
         return "Player wins!";
     }
+}
 
+//Takes the result string and displays it on the HTML. Also updates scores based 
+//the global variables
+function updateDisplay(roundResult) {
+    console.log(roundResult);
+    document.getElementById("results").innerHTML = roundResult;
+    document.getElementById("computer-score").innerHTML = computerScore;
+    document.getElementById("player-score").innerHTML = playerScore;
+    if (computerScore == 5) {
+        alert("Computer wins!")
+    } 
     
-}
-
-//Parameters: Takes roundWinner after running playRound
-//Return: Returns message to player accordingly
-function updateScore(winner, playerSelection, computerSelection) {
-    if (winner === "Tie!") 
-        return `It's a tie!`
-    if (winner === "Computer wins!") 
-        return `Computer wins! ${capitalizeFirstLetter(computerSelection)} beats ${playerSelection.toLowerCase()}`
-    if (winner === "Player wins!") 
-        return `Player wins! ${capitalizeFirstLetter(playerSelection)} beats ${computerSelection.toLowerCase()}`  
-}
-
-//Slice first letter of string, apply toUpperCase, add back rest of string
-function capitalizeFirstLetter (str) {
-    return str.charAt(0).toUpperCase() + str.slice(1).toLowerCase() 
-}
-
-function getPlayerChoice () {
-    return "rock"
-}
-
-//Parameters: None
-//Description: Call function game, to play playRound 5 times, keep score of player & computer, display score at the end of 5 rounds
-//Return: Results of 5 rounds as an array of strings
-
-function game() {
-    winnerArray = []
-    for (let i=0; i < 5; i++){
-        let playerChoice = getPlayerChoice();
-        let computerChoice = getComputerChoice();
-        let roundWinner = playRound(playerChoice.toLowerCase(), computerChoice.toLowerCase());
-        let returnMessage = updateScore(roundWinner, playerChoice, computerChoice);
-        console.log(returnMessage)
-        //Pushes result of playRound into winnerArray above
-        winnerArray.push(roundWinner)
-       
+    else if (playerScore == 5) {
+        alert("Player wins!")
     }
-
-    numOfPlayerWins = winnerArray.filter(x => x === "Player wins!").length
-    console.log("Player: " + numOfPlayerWins)
-
-    numOfComputerWins = winnerArray.filter(x => x === "Computer wins!").length
-    console.log("Computer: " + numOfComputerWins)
-
-    //First tally number of player wins
-    //Then tally number of computer wins
-    //If player is greater than computer, player wins and vice versa
-    //Store the winner of each playRound function
-    //Return a score board
 }
+
+document.getElementById("rock").addEventListener("click", function () {
+    updateDisplay(playRound("rock", getComputerChoice()));
+});
+
+document.getElementById("paper").addEventListener("click", function () { 
+    updateDisplay(playRound("paper", getComputerChoice()));
+    
+});
+
+document.getElementById("scissors").addEventListener("click", function () { 
+    updateDisplay(playRound("scissors", getComputerChoice()));
+});
+
+
+
+//one button event listener, in this case scissors
+/*document.getElementById("scissors").addEventListener("click", function () { 
+    let roundResult = playRound("scissors", getComputerChoice());
+    console.log(roundResult);
+    document.getElementById("results").innerHTML = roundResult;
+    document.getElementById("computer-score").innerHTML = computerScore;
+    document.getElementById("player-score").innerHTML = playerScore;
+}); */
+
+// numOfPlayerWins = winnerArray.filter(x => x === "Player wins!").length
+// console.log("Player: " + numOfPlayerWins)
+
+// numOfComputerWins = winnerArray.filter(x => x === "Computer wins!").length
+// console.log("Computer: " + numOfComputerWins)
+
+//First tally number of player wins
+//Then tally number of computer wins
+//If player is greater than computer, player wins and vice versa
+//Store the winner of each playRound function
+//Return a score board
 
